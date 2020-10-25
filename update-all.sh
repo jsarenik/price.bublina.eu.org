b43d19483fa24fe8832d8f4df3c33a657e7736ce
Jan Sarenik <jajomojo@gmail.com>
2020-10-25 20:38:44 +0100

    Add update-all.sh with cron example

---
+++ update-all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Meant to be run by crontab: 10 5 * * * ssh host
+# where ~/.ssh/authorized_keys on host contain something like
+# command="~/web/bitcoin.zorinaq.com/update-all.sh" ssh-ed25519 AAAAC3Nz...
+
+a="/$0"; a=${a%/*}; a=${a:-.}; a=${a#/}/; BINDIR=$(cd $a; pwd)
+cd $BINDIR
+./update-csv.sh
+./update-ipfs.sh
