40d249f0a6d203eb72be11a55fa91e81478b8cd0
Jan Sarenik <jajomojo@gmail.com>
2020-10-29 19:56:10 +0100

    dnslink.sh: Fix lookup at NS
    Usage: dnslink.sh <domain> [parent_in_NS]
    
    Example: dnslink.sh price.bublina.eu.org
    Example: dnslink.sh deep.sub.dom.bublina.eu.org bublina.eu.org

--- dnslink.sh
+++ dnslink.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+#
+# Usage: dnslink.sh <domain> [parent_in_NS]
+#
+# Example: dnslink.sh price.bublina.eu.org
+# Example: dnslink.sh deep.sub.dom.bublina.eu.org bublina.eu.org
 
-NS=$(dig -t NS +short $1)
+PARENT=${2:-${1#*.}}
+NS=$(dig -t NS +short $PARENT)
 dig +short TXT _dnslink.$1 @$NS \
   | sed -E 's/"dnslink=(.*)"/\1/g'
