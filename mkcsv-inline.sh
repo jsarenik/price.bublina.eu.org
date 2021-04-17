98bd95655e87a7538c497cb3812533f22f1f5746
Jan Sarenik <jajomojo@gmail.com>
2021-04-17 21:34:38 +0200

    mkcsv-inline.sh: Header was removed in CSV

--- mkcsv-inline.sh
+++ mkcsv-inline.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-{ ./mkcsv.sh | head -1; ./mkcsv.sh | sed '1d' | tr -d '[ a-zA-Z"\[()\]'; } \
+{ ./mkcsv.sh | tr -d '[ a-zA-Z"\[()\]]'; } \
   | sed 's/$/\\n/' | tr -d '\n' | sed 's/.*/"&"/;s/\\n"$/"/'
 echo ,
