de0cf27550894e2c7d5f67a563661a9d94199bf2
Jan Sarenik <jajomojo@gmail.com>
2021-07-16 09:21:18 +0200

    hafuch.sh: Better busybox bc handling

--- hafuch.sh
+++ hafuch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-type bc && BC=bc || {
-  wget https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64
-  chmod a+x busybox-x86_64
+type bc >/dev/null 2>&1 && BC=bc || {
+  test -x $PWD/busybox-x86_64 || {
+    wget --quiet \
+      https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64
+    chmod a+x busybox-x86_64
+  }
   BC="$PWD/busybox-x86_64 bc"
 }
 
