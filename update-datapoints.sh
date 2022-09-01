17fcc8da47a5cdbed7630e3c8409cc1af0c6f551
Jan Sarenik <jajomojo@gmail.com>
2022-09-01 12:48:38 +0200

    update-datapoints: Add check in the end

--- update-datapoints.sh
+++ update-datapoints.sh
@@ -52,3 +52,11 @@ test "$EXIT" = "1" && { echo No new data found. Exiting.; exit 1; }
 echo "Updating datapoints-blocks..."
 sed -n "$lines,\$p" datapoints | sed 1d | ./mkcsv-datapoints.sh \
   | tee -a datapoints-blocks | grep . && echo ...datapoints-blocks updated.
+
+# Check if there are more than one lines in datapoints-blocks
+# starting with "^1,"
+LINES=$(grep -c '^1,' datapoints-blocks)
+test "$LINES" = "1" || {
+  # reset last additions
+  git checkout datapoints datapoints-blocks
+}
