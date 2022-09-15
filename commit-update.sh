fcede4ddd0bc32c76f43e2e197ff9a1c92329194
Jan Sarenik <jajomojo@gmail.com>
2022-09-15 15:24:10 +0000

    commit-update.sh: Add random delay up to 2 hours

--- commit-update.sh
+++ commit-update.sh
@@ -2,6 +2,7 @@
 
 a="/$0"; a=${a%/*}; a=${a:-.}; a=${a#/}/; BINDIR=$(cd "$a" || true; pwd)
 cd $BINDIR
+sleep $((RANDOM%120))
 git pull
 ./update-all.sh
 git commit -m "Update datapoints" datapoints*
