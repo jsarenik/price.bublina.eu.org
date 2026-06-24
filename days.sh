bc5bba80bec4e01f39c919bb2a1d4e8aa7406fb6
Ján Sáreník <jajomojo@gmail.com>
2026-06-24 10:07:10 +0200

    days: Fix URL in curl command for data retrieval

--- days.sh
+++ days.sh
@@ -13,7 +13,7 @@ safecat() (
 )
 
 curl -s \
-  "https://data-api.cryptocompare.com/index/cc/v1/historical/days?market=s$marke&instrument=$instr&limit=$limit&aggregate=1&fill=true&apply_mapping=true&response_format=JSON" \
+  "https://data-api.cryptocompare.com/index/cc/v1/historical/days?market=$marke&instrument=$instr&limit=$limit&aggregate=1&fill=true&apply_mapping=true&response_format=JSON" \
   -H 'Accept: application/json, text/plain, */*' \
   -H 'Accept-Language: en-US,en;q=0.9,sk;q=0.8,cs;q=0.7' \
   -H 'Authorization: Bearer undefined' \
